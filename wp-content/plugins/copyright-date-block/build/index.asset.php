ee9b781bca73a2c470ebde1b117ca7a6a0370473
Ford Henley <info@fordswebhelp.com>
2024-11-25 16:53:19 -0600

    completed dynamic block lesson

--- wp-content/plugins/copyright-date-block/build/index.asset.php
+++ wp-content/plugins/copyright-date-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3bae80de33172d27aab4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cd2b652d0dd2fc42a8cc');
